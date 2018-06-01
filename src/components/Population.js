e8b9a4d6e0add6dcf3c50919c3523cb2487e23ba
Awet Fessha <awetkifle94@gmail.com>
2018-06-02 00:37:35 +0100

    first commit

--- src/components/Population.js
+++ src/components/Population.js
@@ -1,43 +1,69 @@
-import React, { Component } from 'react';
-import './Population.css';
+import React, { Component } from "react";
+import "./Population.css";
+import members from "../data/members";
 
 class Population extends Component {
-	render() {
-		return (
-			<div className="Population">
-				<div className="Population-header">
-					<h2>Population of Member Countries</h2>
-					<input type="text" placeholder="Search by country" />
-				</div>
-				<table>
-					<thead>
-						<tr>
-							<th>Name</th>
-							<th>Country</th>
-							<th>Population</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td>Mozafar</td>
-							<td>Sudan</td>
-							<td>39598700</td>
-						</tr>
-						<tr>
-							<td>Daniel</td>
-							<td>United Kingdom</td>
-							<td>65110000</td>
-						</tr>
-					</tbody>
-					<tfoot>
-						<tr>
-							<td colSpan="3">Total Population: ???</td>
-						</tr>
-					</tfoot>
-				</table>
-			</div>
-		)
-	}
+  constructor() {
+    super();
+    this.state = {
+      statisticsList: []
+    };
+
+    fetch("https://restcountries.eu/rest/v2/")
+      .then(data => {
+        return data.json();
+      })
+      .then(serverStatistics => {
+        const membersMatching = members.map(result => {
+          const countryInfo = serverStatistics.find(
+            serverResult => result.country === serverResult.name
+          );
+
+          return {
+            name: result.name,
+            country: result.country,
+            population: countryInfo.population
+          };
+        });
+
+        this.setState({
+          statisticsList: membersMatching
+        });
+      });
+  }
+  render() {
+    return (
+      <div className="Population">
+        <div className="Population-header">
+          <h2>Population of Member Countries</h2>
+          <input type="text" placeholder="Search by country" />
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Country</th>
+              <th>Population</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.statisticsList.map(result => (
+              <tr>
+                <td>{result.name}</td>
+                <td>{result.country}</td>
+                <td>{result.population}</td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan="3">Total Population: </td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default Population;
