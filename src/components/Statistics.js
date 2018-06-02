a592455efa9cad68076a28e900c6f6ff349534bf
mnur86 <mnur_86@icloud.com>
2018-06-02 16:49:22 +0100

    completed total task of members/countries/regions

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,17 +1,48 @@
-import React, { Component } from 'react';
-import Statistic from './Statistic';
-import './Statistics.css';
+import React, { Component } from "react";
+import Statistic from "./Statistic";
+import "./Statistics.css";
+import people from "../data/members.js";
 
 class Statistics extends Component {
-	render() {
-		return (
-			<div className="Statistics">
-				<Statistic label="Total Members" />
-				<Statistic label="Total Countries" />
-				<Statistic label="Total Regions" />
-			</div>
-		)
-	}
+  constructor(props) {
+    super(props);
+    this.state = { regions: [] };
+  }
+
+  countries = people.map(person => person.country);
+  distinctCountries = Array.from(new Set(this.countries));
+
+  regions = [];
+
+  componentDidMount() {
+    this.distinctCountries.map(country => {
+      fetch("https://restcountries.eu/rest/v2/name/" + country)
+        .then(data => data.json())
+        .then(response => {
+          this.regions.push(response[0].region);
+          this.setState({
+            regions: this.regions
+          });
+        });
+    });
+  }
+
+  getRegions() {
+    return Array.from(new Set(this.state.regions));
+  }
+
+  render() {
+    return (
+      <div className="Statistics">
+        <Statistic total={people.length} label="Total Members" />
+        <Statistic
+          total={this.distinctCountries.length}
+          label="Total Countries"
+        />
+        <Statistic total={this.getRegions().length} label="Total Regions" />
+      </div>
+    );
+  }
 }
 
 export default Statistics;
