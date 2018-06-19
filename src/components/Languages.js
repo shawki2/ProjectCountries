1fb91db19b06057cfc59d83fa8ba0186461800a1
Shawki Al-Dubaee <shawki@shawkis-mbp.home>
2018-06-19 08:22:46 +0100

    Fix TotalLanguages to InputBox and delete Statistics222 file

--- src/components/Languages.js
+++ src/components/Languages.js
@@ -4,11 +4,11 @@ import "./Languages.css";
 class Languages extends React.Component {
   getTotalLang() {
     const languagesList = this.props.filterStatisticsList
-    .filter(list => list.country.includes(this.props.inputBoxLang))
-    .reduce((result, item) => {
-      const languages = item.languages.split(", ");
-      return result.concat(languages);
-    }, []);
+      .filter(list => list.languages.includes(this.props.inputBoxLang))
+      .reduce((result, item) => {
+        const languages = item.languages.split(", ");
+        return result.concat(languages);
+      }, []);
 
     return [...new Set(languagesList)];
 
@@ -20,14 +20,16 @@ class Languages extends React.Component {
     // return distinctLanguages
   }
   getInputLang() {
-    var languagesFilter = this.props.statisticsList.filter(
-      list => list.country.includes(this.props.inputBoxLang.value)).map(country => country.languages);
+    var languagesFilter = this.props.filterStatisticsList.filter(
+      list => list.languages.includes(this.props.inputBoxLang.value)).map(
+      country => country.languages);
     var distinctLanguages = [...(new Set(languagesFilter))];
     return distinctLanguages
     //until here for languages companents
   }
 
   render() {
+    console.log(this.getTotalLang())
     return (
       <div className="Languages">
         <div className="Languages-header">
@@ -50,7 +52,7 @@ class Languages extends React.Component {
           </thead>
           <tbody>
             {this.props.filterStatisticsList
-            .filter(list => list.country.includes(this.props.inputBoxLang))
+              .filter(list => list.languages.includes(this.props.inputBoxLang))
               .sort((a, b) => b.languageCount - a.languageCount)
               .map((result, index) => (
                 <tr key={index}>
@@ -63,6 +65,7 @@ class Languages extends React.Component {
           <tfoot>
             <tr>
               <td colSpan="3">Total Languages: {this.getTotalLang().length}</td>
+              {console.log(this.getTotalLang())}
             </tr>
           </tfoot>
         </table>
